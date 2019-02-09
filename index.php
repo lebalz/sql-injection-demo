d5c0df842a8805a40aaee330675c4344b8519415
Balz <lebalz@outlook.com>
2019-02-09 12:49:36 +0100

    add comments, use bootstrap

--- index.php
+++ index.php
@@ -6,16 +6,21 @@
 </head>
 
 <body>
-  <h1>Onlineshop Kaffee</h1>
-  <form action="index.php" method="get">
-    Blend: <input type="text" name="blend"><br>
-    <input type="submit">
-  </form>
   <?php
-      require_once('showTable.php');
-      showTable(array_key_exists('blend', $_GET) ? $_GET['blend'] : '');
-    ?>
-  </table>
+    $filter = array_key_exists('blend', $_GET) ? $_GET['blend'] : ''
+  ?>
+  <h1>Onlineshop Kaffee</h1>
+  <form  action="index.php" method="get">
+  <div class="form-group">
+    <label for="blendFilter">Blend Name</label>
+    <input type="text" class="form-control" id="blendFilter" name="blend"
+           placeholder="Filter Blend Names" value=<?php echo($filter) ?>>
+  </div>
+  <button type="submit" class="btn btn-primary">Filtern</button>
+</form>
+  <?php
+    require_once('showTable.php');
+    showTable($filter);
+  ?>
 </body>
-
 </html>
--- showTable.php
+++ showTable.php
@@ -1,44 +1,56 @@
 <?php
-function showTable($blend)
+function showTable($blend_name)
 {
   ?>
   <table class="table border">
-    <tr><th>id</th><th>Blend</th><th>Origin</th><th>Variety</th><th>Notes</th><th>Intensifier</th><th>Price</th></tr>
+    <tr>
+      <th>id</th>
+      <th>BlendName</th>
+      <th>Origin</th>
+      <th>Variety</th>
+      <th>Notes</th>
+      <th>Intensifier</th>
+      <th>Price</th>
+    </tr>
     <?php
-    require_once('connectdb.php');
-    $db = connectdb();
-    $query = "SELECT * FROM coffee WHERE blend_name LIKE '%". $blend . "%';";
+      require_once('connectdb.php');
+      // vulnerability to sql injection
+      $query = "SELECT * FROM coffee WHERE blend_name LIKE '%". $blend_name . "%';";
     ?>
-    <h6>Query:</h6>
-    <code>
-      <?php
-        echo($query);
-      ?>
-    </code>
     <br>
+    <div class="card">
+      <div class="card-body">
+        Query: <code>
+          <?php
+            echo($query);
+          ?>
+        </code>
+      </div>
+    </div>
     <br>
     <h3>Kaffee</h3>
     <?php
-    $result = mysqli_multi_query($db, $query);
-    if ($result) {
-      $result = mysqli_use_result($db);
-    }
-    if ($result) {
-      while ($coffee = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-        echo ('<tr>');
-        foreach ($coffee as $attr) {
-          echo ('<td>' . $attr . '</td>');
-        }
-        echo ('</tr>');
+      $db = connectdb();
+      // mysqli_multi_query is required. With mysqli_query only the first
+      // query statement is performed to prevent sql injection...
+      $result = mysqli_multi_query($db, $query);
+      if ($result) {
+        $result = mysqli_use_result($db);
       }
-    } else {
-      echo('ERROR: not working!');
-    }
-    mysqli_close($db);
+      if ($result) {
+        while ($coffee = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+          echo ('<tr>');
+          foreach ($coffee as $attr) {
+            echo ('<td>' . $attr . '</td>');
+          }
+          echo ('</tr>');
+        }
+      } else {
+        echo('ERROR: not working!');
+      }
+      mysqli_close($db);
     ?>
   </table>
-
-<?php
-
+  <?php
 }
 ?>
