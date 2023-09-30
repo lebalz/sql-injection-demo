758c4c367f11e0c3e198d5081f6dd9f10d9f802f
LeBalz <lebalz@outlook.com>
2023-09-30 11:34:27 +0200

    add optional script tag

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 
 <head>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <?php
+  if (getenv('SCRIPT_TAG')) {
+    echo (getenv('SCRIPT_TAG'));
+  }
+  ?>
 </head>
 
 <body>
